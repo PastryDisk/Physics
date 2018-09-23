43ab2bbf4cf8e40cdfba8822dee545f3667e3e13
kmasiak <38893432+kmasiak@users.noreply.github.com>
2018-09-22 23:09:05 -0400

    Working with 3 formulas

--- Physics/src/physics/Physics.java
+++ Physics/src/physics/Physics.java
@@ -31,7 +31,43 @@ public class Physics {
             System.out.println(vars[i]);
         }
         
+        onFormula(vars);
+        
+    }
+   
+    
+    public static void onFormula(String[] variables){
+        int[] formulas = {0, 0, 0};
+        for (int i = 0; i < variables.length; i++) {
+            
+            if (variables[i].equals("v") || variables[i].equals("v0") || variables[i].equals("a") || variables[i].equals("t")) {
+                formulas[0]++;
+            }
+            
+            if (variables[i].equals("x") || variables[i].equals("x0") || variables[i].equals("v0") || variables[i].equals("t") || variables[i].equals("a")) {
+                formulas[1]++;
+            }
+            
+            if (variables[i].equals("v") || variables[i].equals("v0") || variables[i].equals("a") || variables[i].equals("x") || variables[i].equals("x0")) {
+                formulas[2]++;
+            }       
+        }
+        
+        
+        if (formulas[0] == 3) {
+            System.out.println("v = v0 + at");
+        }
+        
+        if (formulas[1] == 4) {
+            System.out.println("x = x0 + v0t + 0.5at2");
+        }
+        
+        if (formulas[0] == 4) {
+            System.out.println("v2 = v02 + 2a(x - x0)");
+        }
         
     }
     
 }
+
+ 
